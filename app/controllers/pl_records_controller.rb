8610b0b34c5c4e7e0803729d40641fc7cf1ea13c
rajashekarguda <rajashekarguda@gmail.com>
2019-07-23 22:54:30 +0530

    removed un used files

--- app/controllers/pl_records_controller.rb
+++
@@ -1,3 +0,0 @@
-class PlRecordsController < CrudController
-	
-end
--- app/models/db_group_obj.rb
+++
@@ -1,9 +0,0 @@
-class DbGroupObj < ApplicationRecord
-
-	belongs_to :db_group
-	
-	def obj
-		send db_group.obj_type.underscore.to_sym
-	end
-	
-end
