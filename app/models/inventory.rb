4cce0d572a52e81e2046e077a52b2e7f80415f3b
rajashekarguda <rajashekarguda@gmail.com>
2019-12-20 23:41:40 +0530

    issues are fixed

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,4 +1,23 @@
 class Inventory < ApplicationRecord
 	validates_presence_of :item_dec
-	validates_format_of :incident_rep, :with => /\A[0-9][0-9_]*\Z/, message: "Please enter numbers only"
+	validates_uniqueness_of :item_dec, message: "Please enter Description", if: :uniqe_item_dec
+	validates_format_of :incident_rep, :with => /\A[0-9][0-9_]*\Z/, message: "Please enter numbers only", if: :is_non_seriral?
+
+	def is_non_seriral?
+		bool = false
+		if(nsn_in_inventory)
+		  	bool = (nsn_in_inventory.to_i >= 0)
+		else
+			bool = nsn_in_inventory
+		end
+		!!bool
+	end
+
+	def uniqe_item_dec
+		if(nsn_in_inventory)
+			Inventory.where(item_dec: item_dec).where("nsn_in_inventory is not null").length > 0
+		else
+			Inventory.where(item_dec: item_dec).where("nsn_in_inventory is null").length > 0
+		end
+	end
 end
