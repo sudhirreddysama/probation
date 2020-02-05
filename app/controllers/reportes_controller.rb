a1f8bd8a11adfdef6d123f3582bf0c10794b4b73
rajashekarguda <rajashekarguda@gmail.com>
2020-02-05 22:03:05 +0530

    item_dec report is fixed

--- app/controllers/reportes_controller.rb
+++ app/controllers/reportes_controller.rb
@@ -13,4 +13,10 @@ class ReportesController < CrudController
   	session["report"] = {type: "agent_records", data: params[:data]}
     render partial: "agent_records"
   end
+
+  def get_items_from_item_dec
+  	@items = Inventory.where(item_dec: params["data"])
+  	session["report"] = {type: "Inv_assign_report", data: params[:data]}
+    render partial: "inv_assign_report"
+  end
 end
